98769a6a1f6d73637fe08e0dd8d1b590940e1201
Ahmed <1alnjm1a@gmail.com>
2026-08-03 02:41:40 +0300

    edit cors

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ const adminRouter = require("./routers/admin.route");
 const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://masar-flame.vercel.app/"],
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "10kb" }));
 app.use((req, res, next) => {
   // تنظيف الـ body والـ params بشكل طبيعي
