0b3341c29e526f5393443028493b6c88104ecef9
Ahmed <1alnjm1a@gmail.com>
2026-08-03 02:35:34 +0300

    remote db

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
   port: process.env.PORT || 3001,
-  mongoUrl: process.env.MONGO_URL_LOCAL,
+  mongoUrl: process.env.MONGO_URL,
   jwtSecret: process.env.JWT_SECRET_KEY,
   jwtExpire: "7d",
   theRateLimit: {
