df4beb39229d18baf5608c48a9fbbd7a8243d835
Ahmed <1alnjm1a@gmail.com>
2026-08-03 03:13:21 +0300

    edit cors

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://masar-flame.vercel.app/"],
+    origin: ["http://localhost:5173", "https://masar-flame.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
+app.options("*", cors());
 app.use(express.json({ limit: "10kb" }));
 app.use((req, res, next) => {
   // تنظيف الـ body والـ params بشكل طبيعي
